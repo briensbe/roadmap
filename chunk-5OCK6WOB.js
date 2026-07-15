e8953525c3c0cf60583bb8d7ac717cf61bb12ab4
briensbe <briensbe@yahoo.com>
2026-07-15 22:51:09 +0200

    Auto-generated commit

--- chunk-5OCK6WOB.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-YP33BE73.js";import"./chunk-IAADSV54.js";import"./chunk-QBEH2WCK.js";import"./chunk-2NWMI7JW.js";export{a as AdministrationComponent};
---
+++ chunk-WPCUKAYZ.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-456FWOJR.js";import"./chunk-L6GAHUEX.js";import"./chunk-NAXJSNFM.js";import"./chunk-KB77IILZ.js";export{a as AdministrationComponent};
