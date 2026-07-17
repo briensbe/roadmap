8ba2e119b89a96254a642117f9d1e7a534e1bead
briensbe <briensbe@yahoo.com>
2026-07-17 19:03:38 +0200

    Auto-generated commit

---
+++ chunk-43LHMWXB.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-YWIFAUFE.js";import"./chunk-UTUOGBCP.js";import"./chunk-QKB3DYKU.js";import"./chunk-ZIKGANJL.js";export{a as AdministrationComponent};
--- chunk-WPCUKAYZ.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-456FWOJR.js";import"./chunk-L6GAHUEX.js";import"./chunk-NAXJSNFM.js";import"./chunk-KB77IILZ.js";export{a as AdministrationComponent};
