26a923874e5a2249c441f279978c1db5bcc7c2f3
briensbe <briensbe@yahoo.com>
2026-08-28 19:51:57 +0200

    Auto-generated commit

---
+++ chunk-472JPEMO.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-XGGXVWBG.js";import"./chunk-ZJWIEEXC.js";import"./chunk-REMSTJDS.js";import"./chunk-ROQYQYTS.js";export{a as AdministrationComponent};
--- chunk-BQYK2DRX.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-7L6R3NE7.js";import"./chunk-ZKNIUJUU.js";import"./chunk-XF5EXUO2.js";import"./chunk-HUUKJAG4.js";export{a as AdministrationComponent};
